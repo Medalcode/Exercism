a88d5b6080d4568ff1b8301e63b91f923a5aa22f
Jonatthan Medalla <jonatthan.medalla@inacapmail.cl>
2025-01-23 19:11:32 -0300

    7

--- Python/blackjack.py
+++ Python/blackjack.py
@@ -18,4 +18,42 @@ def value_of_card(card):
 
     return card
 
+def higher_card(card_one, card_two):
+    """Determine which card has a higher value in the hand.
 
+    :param card_one, card_two: str - cards dealt in hand.  See below for values.
+    :return: str or tuple - resulting Tuple contains both cards if they are of equal value.
+
+    1.  'J', 'Q', or 'K' (otherwise known as "face cards") = 10
+    2.  'A' (ace card) = 1
+    3.  '2' - '10' = numerical value.
+    """
+    
+    if card_one > card_two:
+        return card_one
+    elif card_two > card_one:
+        return card_one
+    else:
+        return card_one, card_two
+
+
+def value_of_ace(card_one, card_two):
+    """Calculate the most advantageous value for the ace card.
+
+    :param card_one, card_two: str - card dealt. See below for values.
+    :return: int - either 1 or 11 value of the upcoming ace card.
+
+    1.  'J', 'Q', or 'K' (otherwise known as "face cards") = 10
+    2.  'A' (ace card) = 11 (if already in hand)
+    3.  '2' - '10' = numerical value.
+    """
+    value_one = value_of_card(card_one)
+    value_two = value_of_card(card_two)
+    
+    if card_one == 'A' or card_two == 'A':
+        if value_one == value_of_card(card_one) + value_two == value_of_card(card_two) > 10:
+            return 1
+        else:
+            return 11
+    else:
+        return 11
