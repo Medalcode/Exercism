85bbe61bd264e5b0bedc97b556a1fffdf47f690b
Jonatthan Medalla <jonatthan.medalla@inacapmail.cl>
2025-01-27 02:58:18 -0300

    14

---
+++ Python/strings.py
@@ -0,0 +1,45 @@
+"""Functions for creating, transforming, and adding prefixes to strings."""
+
+
+def add_prefix_un(word):
+    """Take the given word and add the 'un' prefix.
+
+    :param word: str - containing the root word.
+    :return: str - of root word prepended with 'un'.
+    """
+    return "un"+word
+
+def make_word_groups(vocab_words):
+    """Transform a list containing a prefix and words into a string with the prefix followed by the words with prefix prepended.
+
+    :param vocab_words: list - of vocabulary words with prefix in first index.
+    :return: str - of prefix followed by vocabulary words with
+            prefix applied.
+
+    This function takes a `vocab_words` list and returns a string
+    with the prefix and the words with prefix applied, separated
+     by ' :: '.
+
+    For example: list('en', 'close', 'joy', 'lighten'),
+    produces the following string: 'en :: enclose :: enjoy :: enlighten'.
+    """
+
+    if vocab_words[0] == 'en':
+        return ' :: en'.join(vocab_words)
+    elif vocab_words[0] == 'pre':
+        return ' :: pre'.join(vocab_words)
+    elif vocab_words[0] == 'auto':
+        return ' :: auto'.join(vocab_words)
+    else:
+        return ' :: inter'.join(vocab_words)
+
+
+def remove_suffix_ness(word):
+    """Remove the suffix from the word while keeping spelling in mind.
+
+    :param word: str - of word to remove suffix from.
+    :return: str - of word with suffix removed & spelling adjusted.
+
+    For example: "heaviness" becomes "heavy", but "sadness" becomes "sad".
+    """
+    
