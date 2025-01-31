1b4cc063b46f0874729b06b050e434aaeabf42ab
Jonatthan Medalla <jonatthan.medalla@inacapmail.cl>
2025-01-31 13:05:43 -0300

    21

--- Javascript/aves.js
+++ Javascript/aves.js
@@ -13,21 +13,44 @@
 
 export function totalBirdCount(birdsPerDay) {
 
-    let contador = 0;
-      for (let i = 0; i < birdsPerDay.length; i++){
-        console.log(birdsPerDay[i])
-        contador +=  birdsPerDay[i]
-        console.log(contador)
-      }
-    return contador
-  }
-  
-  
-  export function birdsInWeek(birdsPerDay, week) {
+  let contador = 0;
+    for (let i = 0; i < birdsPerDay.length; i++){
+      console.log(birdsPerDay[i])
+      contador +=  birdsPerDay[i]
+      console.log(contador)
+    }
+  return contador
+}
+
+/**
+ * Calculates the total number of birds seen in a specific week.
+ *
+ * @param {number[]} birdsPerDay
+ * @param {number} week
+ * @returns {number} birds counted in the given week
+ */
+export function birdsInWeek(birdsPerDay, week) {
     let first_day = (week - 1) * 7;
     let contador = 0;
     for (let i = first_day; i < first_day + 7; i++) {
         contador += birdsPerDay[i];
     }
     return contador;
+}
+
+/**
+ * Fixes the counting mistake by increasing the bird count
+ * by one for every second day.
+ *
+ * @param {number[]} birdsPerDay
+ * @returns {number[]} corrected bird count data
+ */
+export function fixBirdCountLog(birdsPerDay) {
+  let contador = 0;
+    for (let i = 0; i < birdsPerDay.length; i++){
+      console.log(birdsPerDay[i])
+      contador +=  birdsPerDay[i]
+      console.log(contador)
+    }
+    return contador
 }
