d336a87bf0b483d5d7d0b8372415911805b86383
Jonatthan Medalla <jonatthan.medalla@inacapmail.cl>
2025-02-02 15:04:52 -0300

    23

--- Python/listas.py
+++ Python/listas.py
@@ -17,10 +17,14 @@ def concatenate_rounds(rounds_1, rounds_2):
     return rounds_1 + rounds_2
 
 
+def list_contains_round(rounds, number):
+    if number in rounds:
+        return True
+    else:
+        return False
 
-hand = [ 1, 2, 3, 4]
 
-def approx_average_is_average(hand):
+def card_average(hand):
     print("hand=",hand)
     suma = 0
     for i in range(len(hand)):
@@ -32,19 +36,36 @@ def approx_average_is_average(hand):
         print("promedio=",promedio)
 
     return promedio
+        
 
-hand = [ 1, 2, 3, 4, 5]
-
-def average_even_is_average_odd(hand):
+def approx_average_is_average(hand):
     suma_real = sum(hand)
     promedio_real = suma_real / len(hand)
-    promedio = hand[0] + hand[-1] // 2
-    mediana = hand[len(hand)//2] 
-    print("promedio=",promedio)
-    print("mediana=",mediana)
-    if  promedio == promedio_real or mediana == promedio_real:
-        return True
-    else:
-        return False
+    
+    promedio_extremos = (hand[0] + hand[-1]) / 2
+
+    mediana = hand[len(hand) // 2]
+    
+    return promedio_extremos == promedio_real or mediana == promedio_real
+
+def average_even_is_average_odd(hand):
+    contador_par = 0
+    suma_par = 0
+    contador_impar = 0
+    suma_impar = 0
+
+    for i in range(len(hand)):
+        if i % 2 == 0:
+            contador_par += 1
+            suma_par += hand[i]
+        else:
+            contador_impar += 1
+            suma_impar += hand[i]
+
+    promedio_par = suma_par / contador_par if contador_par != 0 else 0
+    promedio_impar = suma_impar / contador_impar if contador_impar != 0 else 0
+
+    return promedio_par == promedio_impar
 
 
+def maybe_double_last(hand):
